8cae5add02e1b0bc79fd64757f181b6a4478e2a8
aivl2018 <45841067+aivl2018@users.noreply.github.com>
2019-03-11 09:20:24 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,2 +1,2 @@
-def add(x,y):passdef subtract(x,y):passdef multiply(x,y):passdef divide(x,y):
-passdef square(x,y):pass
+def add(x,y):passdef subtract(x,y):passdef multiply(x,y):passdef subtract(x,y):
+passdef square(x,y):x+y
