49bcfc576e3e35c149ab88fc3ae118df5ffb02b2
aivl2018 <bhushankale444@gmail.com>
2019-09-28 07:44:21 +0000

    pp

--- calc.py
+++ calc.py
@@ -1,3 +1,3 @@
-def add(x,y):passdef subtract(x,y):passdef multiply(x,y):passdef subtract(x,y):
+iidef add(x,y):passdef subtract(x,y):passdef multiply(x,y):passdef subtract(x,y):
 passdef square(x,y):x+y
 Bhushan Kale
